223d0c19ac77d82d157fbeeb64c712c5d4018d15
FPTShop <FPTShop@LAPTOP-E6O8DUU0>
2023-11-04 10:23:46 +0700

    zip

---
+++ ZIP/src/Main.java
@@ -0,0 +1,13 @@
+import controller.FileCompressionController;
+import view.FileCompressionView;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        FileCompressionView view = new FileCompressionView();
+        FileCompressionController controller = new FileCompressionController(view);
+
+        controller.start();
+    }
+}
